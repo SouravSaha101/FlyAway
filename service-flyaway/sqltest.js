b3d3b094d722dd5a2871c2fcbe61002b7d723686
Sourav <souravsaha101@gmail.com>
2021-09-05 01:31:04 +0530

    Airport table added

--- service-flyaway/sqltest.js
+++ service-flyaway/sqltest.js
@@ -5,21 +5,21 @@ const config = {
   authentication: {
     options: {
       userName: "User", // update me
-      password: "Password1" // update me
+      password: "Password1", // update me
     },
-    type: "default"
+    type: "default",
   },
   server: "test-node-dev.database.windows.net", // update me
   options: {
     database: "dev-flyaway-db", //update me
-    encrypt: true
-  }
+    encrypt: true,
+  },
 };
 
 const connection = new Connection(config);
 
 // Attempt to connect and execute queries if connection goes through
-connection.on("connect", err => {
+connection.on("connect", (err) => {
   if (err) {
     console.error(err.message);
   } else {
@@ -34,7 +34,7 @@ function queryDatabase() {
 
   // Read all rows from table
   const request = new Request(
-    `select GETDATE()`,
+    `SELECT * FROM [dbo].[AIRPORTS]`,
     (err, rowCount) => {
       if (err) {
         console.error(err.message);
@@ -44,10 +44,12 @@ function queryDatabase() {
     }
   );
 
-  request.on("row", columns => {
-    columns.forEach(column => {
+  request.on("row", (columns) => {
+    console.log("(");
+    columns.forEach((column) => {
       console.log("%s\t%s", column.metadata.colName, column.value);
     });
+    console.log(")");
   });
 
   connection.execSql(request);
